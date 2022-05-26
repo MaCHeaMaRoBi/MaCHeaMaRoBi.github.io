da6b49876cd70a7c750849884262e8de81719316
MaCHeaMaRoBi <65917625+MaCHeaMaRoBi@users.noreply.github.com>
2022-05-26 23:11:11 +0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const newImg = document.querySelector("#dogImg");
 const dogP = document.querySelector("#dogP");
+const btn = document.querySelector("#btn");
 
-document.querySelector("#btn").addEventListener("click", async () => {
+btn.addEventListener("click", async () => {
   var audio = new Audio("./sad.mp3");
   audio.play();
-
+  btn.classList.add("disabled");
   await delay("It's always", "./happy-dog.jpg", 2000);
   await delay("But never...", "./sad-dog.jpg", 3500);
   await delay("", "", 7500);
+  btn.classList.remove("disabled");
 });
 
 const delay = (p, src, delay) => {
